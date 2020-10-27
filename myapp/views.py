bf68751d80491e60c05ea3bafe67da1046df3232
unknown <ninadmsn@outlook.com>
2020-10-27 19:49:36 +0530

    image rendering page example added and loops and conditional statement functionality examples added

--- myapp/views.py
+++ myapp/views.py
@@ -23,3 +23,26 @@ def intro(request,name,age):
 def myfirstpage(request):
     # template = loader.get_template('index.html')
     return render(request,'index.html')
+
+def mysecondpage(request):
+    return render(request,'second.html')
+
+def mythirdpage(request):
+    sentence = "Hello World"
+    greet = "How you doing ?"
+    fruits = ["apple","mango","banana"]
+    num1 , num2 = 10,15
+    ans = num1 > num2
+    print(ans)
+    mydictonary = {
+        "sentence":sentence,
+        "msg":greet,
+        "myfruits":fruits,
+        "myans":ans,
+        "num1":num1,
+        "num2":num2
+    }
+    return render(request,'third.html',context=mydictonary)
+
+def myimagepage(request):
+    return render(request,'image.html')
